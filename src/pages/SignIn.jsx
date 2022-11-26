8b6b8b134e42d1df4c1c2319221354b2ced0b814
Jeffrey <aligula.jeff@gmail.com>
2022-11-26 14:25:40 +0300

    change signin & error page bg colors

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function signin() {
   return (
-    <div className="bg-gray-50 dark:bg-gray-900">
+    <div className="bg-gray-50 dark:bg-[#162daa]">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div
           href="#"
